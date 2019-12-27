bb3fba78e061cb3960700712739c4536fbb487b3
brt2 <brt2@qq.com>
2019-12-27 14:02:16 +0800

    Important！终于实现了更改顶层目录

--- submodules.sh
+++ submodules.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# Usage: submodule [push|pull]
 
-function sub_module_update () {
+test -z $1 && action="pull" || action=$1
+
+# m_utils="git@gitee.com:brt2/utils.git"
+# m_mvlib="git@gitee.com:brt2/mvlib.git"
+# git remote add "m-${dir_module}" $m_utils
+
+function sub_module_pull () {
 dir_module=$1
 url_repo=$2
 branch=$3
-
-    # 添加remote
-    # git remote add "m-${dir_module}" $url_repo
 
     if [ -d $dir_module ]; then
         git subtree pull --prefix=$dir_module $url_repo $branch --squash
@@ -18,5 +22,19 @@ branch=$3
     fi
 }
 
-sub_module_update src/utils/ git@gitee.com:brt2/utils.git dev
-sub_module_update src/mvlib/ git@gitee.com:brt2/mvlib.git dev
+function sub_module_push () {
+dir_module=$1
+url_repo=$2
+branch=$3
+
+    git subtree push --prefix=$dir_module $url_repo $branch
+}
+
+
+if [ $action == "push" ]; then
+    sub_module_push src/utils/ $m_utils dev
+    sub_module_push src/mvlib/ $m_mvlib dev
+else
+    sub_module_pull src/utils/ $m_utils dev
+    sub_module_pull src/mvlib/ $m_mvlib dev
+fi
