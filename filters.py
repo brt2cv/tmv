ca6398bd21a5d08f476f237daeaba0467c0712de
Bright@E49 <brt2@qq.com>
2020-02-07 14:21:09 +0800

    Squashed 'src/mvlib/' changes from c716c75..92aff83
    92aff83 实现图像在打开时自动缩放（用以提高操作的响应速度）
    7b03ae7 删除mvlib.exposure模块
    641c678 增加亮度、对比度、直方图标准化；将直方图显示与直方图标准化工具统一为一个插件
    c8251ca 将Plugins.Demo模块移动到core.plugin下面（仅用于demo，而不会作为Plugins载入）；增加ThresholdPlus插件，高亮0与255的极值区域；更新TestPlugin测试按钮（通过继承测试class而无需改动menu.json配置文件）
    791b002 增加sobel梯度算子等mvlib形态学操作；增加format检测选项“backend”，并更新了IpsFormat的检查规则
    b484eb8 bugfix: 修复Windows缺失HOME环境变量时mvlib配置文件目录错误的问题
    5827995 提供cnts_max()直接获取轮廓极值
    
    git-subtree-dir: src/mvlib
    git-subtree-split: 92aff831ad7ae21229a690d0bb4e035f2ce936f4

--- filters.py
+++ filters.py
@@ -5,8 +5,8 @@ if include("opencv"):
     import cv2
 if include("skimage"):
     import skimage
-# if include("scipy"):
-#     from scipy import ndimage
+if include("scipy"):
+    import scipy.ndimage as nimg
 if include("numpy"):
     import numpy as np
 if include("pillow"):
@@ -73,7 +73,83 @@ def threshold(im, thresh="otsu", maxval=255):
             func_numpy=run_numpy
         )()
 
+def graystairs(im, low_val, high_val):
+    def run_numpy():
+        im2 = np.subtract(im, low_val, casting="unsafe")
+        k = 255 / max(high_val - low_val, 1e-10)
+        im2 = np.multiply(im2, k, casting='unsafe').astype("uint8")
+        im2[im < low_val] = 0
+        im2[im > high_val] = 255
+        return im2
 
+    return run_backend(
+            func_numpy=run_numpy
+        )()
+
+def contrast(im, bright, contrast):
+    def run_numpy():
+        mid = 128 - bright
+        length = 255 / np.tan(contrast / 180 * np.pi)
+
+        im2 = im.copy()
+        if mid - length / 2 > 0:
+            np.subtract(im2, mid-length / 2, out=im2, casting='unsafe')
+            np.multiply(im2, 255 / length, out=im2, casting='unsafe')
+        else:
+            np.multiply(im2, 255 / length, out=im2, casting='unsafe')
+            np.subtract(im2, (mid - length / 2) / length * 255, out=im2, casting='unsafe')
+        im2[im < mid-length/2] = 0
+        im2[im > mid+length/2] = 255
+        return im2
+
+    return run_backend(
+            func_numpy=run_numpy
+        )()
+
+bright = contrast
+
+#####################################################################
+def histogram(im, nBins=256):
+    def run_numpy():
+        return np.histogram(im, range(0, nBins))[0]
+
+    # def run_skimage():
+    #     """ 会自动省略起始和末尾的0列 """
+    #     return skimage.exposure.histogram(im, range(0, nBins))
+
+    def run_opencv():
+        hist = cv2.calcHist([im],[0],None,[256],[0,255])
+        return hist
+
+    return run_backend(
+            # func_skimage=run_skimage,
+            func_opencv=run_opencv,
+            func_numpy=run_numpy
+        )()
+
+def _hist_like(hist1, hist2):
+    hist1 = np.cumsum(hist1) / hist1.sum()
+    hist2 = np.cumsum(hist2) / hist2.sum()
+    hist = np.zeros(256, dtype=np.uint8)
+    i1, i2 = 0, 0
+    while i2 < 256:
+        while i1 < 256 and hist2[i2] > hist1[i1]:
+            i1 += 1
+        hist[i2] = i1
+        i2 += 1
+    return hist
+
+def hist_normalize(im):
+    def run_numpy():
+        hist = histogram(im, 257)
+        ahist = _hist_like(np.ones(256), hist)
+        return ahist[im]
+
+    return run_backend(
+            func_numpy=run_numpy
+        )()
+
+#####################################################################
 def gaussian(im, sigma):
     """
     sigma: scalar or sequence of scalars
@@ -89,11 +165,15 @@ def gaussian(im, sigma):
             sigma_x, sigma_y = sigma
         return cv2.GaussianBlur(im, None, sigma_x, sigmaY=sigma_y)
 
+    def run_scipy():
+        return nimg.gaussian_filter(im, sigma)
+
     def run_skimage():
         return skimage.filters.gaussian(im, sigma)
 
     return run_backend(
             func_opencv=run_opencv,
+            func_scipy=run_scipy,
             func_skimage=run_skimage
         )()
 
@@ -103,12 +183,19 @@ def median(im, k: int):
         k_val = k if k % 2 else k+1
         return cv2.medianBlur(im, k_val)
 
+    def run_scipy():
+        return nimg.median_filter(im, k)
+
     def run_skimage():
-        k_nal = kernal(k, "square")  # 功能被限制了……
+        if isinstance(k, int):
+            k_nal = kernal(k, "square")
+        else:
+            k_nal = kernal(k)
         return skimage.filters.median(im, k_nal)
 
     return run_backend(
             func_opencv=run_opencv,
+            func_scipy=run_scipy,
             func_skimage=run_skimage
         )()
 
@@ -116,14 +203,106 @@ def mean(im, k: tuple):
     def run_opencv():
         return cv2.blur(im, k)
 
+    def run_scipy():
+        return nimg.uniform_filter(im, k)
+
     return run_backend(
-            func_opencv=run_opencv
+            func_opencv=run_opencv,
+            func_scipy=run_scipy
         )()
 
-def filter2D(im, k):
-    def run_opencv(ksize):
-        return cv2.filter2D(im, -1, ksize)
+# def filter2D(im, k):
+#     def run_opencv(ksize):
+#         return cv2.filter2D(im, -1, ksize)
+
+#     return run_backend(
+#             func_opencv=run_opencv
+#         )()
+
+# def laplace(im, uniform=False):
+#     def run_scipy():
+#         im2 = nimg.laplace(im)
+#         im2 *= -1
+#         if uniform:
+#             im2 = np.add(im2, np.mean(ips.range))
+#         return im2
+
+#     return run_backend(
+#             # func_opencv=run_opencv,
+#             func_scipy=run_scipy
+#         )()
+
+# def DOG(im, sigma, sigma2, uniform=False):
+
+# def gaussian_laplace(im, sigma, uniform=False):
+#     def run_scipy():
+#         im2 = nimg.gaussian_laplace(im, sigma)
+#         im2 *= -1
+#         if uniform:
+#             im2 = np.add(im2, np.mean(ips.range))
+#         return im2
+
+#     return run_backend(
+#             # func_opencv=run_opencv,
+#             func_scipy=run_scipy
+#         )()
+
+def maximum(im, k):
+    """ k: int or tuple """
+    def run_scipy():
+        return nimg.maximum_filter(im, k)
 
     return run_backend(
-            func_opencv=run_opencv
+            # func_opencv=run_opencv,
+            func_scipy=run_scipy
+        )()
+
+def minimum(im, k):
+    """ k: int or tuple """
+    def run_scipy():
+        return nimg.minimum_filter(im, k)
+
+    return run_backend(
+            # func_opencv=run_opencv,
+            func_scipy=run_scipy
+        )()
+
+def percentile(im, k):
+    def run_scipy():
+        return nimg.percentile_filter(im, k)
+
+    return run_backend(
+            # func_opencv=run_opencv,
+            func_scipy=run_scipy
+        )()
+
+def prewitt(im, axis: int):
+    orientation = {'horizontal':0, 'vertical':1}
+    if isinstance(axis, str):
+        axis = orientation[axis]
+
+    def run_scipy():
+        return nimg.prewitt(im, axis)
+
+    return run_backend(
+            # func_opencv=run_opencv,
+            func_scipy=run_scipy
+        )()
+
+def sobel(im, axis: int):
+    orientation = {'horizontal':0, 'vertical':1, "both":2}
+    if isinstance(axis, str):
+        axis = orientation[axis]
+
+    def run_scipy():
+        if axis == 2:
+            im_ = im + np.abs(nimg.sobel(im, 0))
+            im2 = im_ + np.abs(nimg.sobel(im_, 1))
+        else:
+            im2 = np.abs(nimg.sobel(im, axis))
+        return im2 // 4
+
+    return run_backend(
+            # func_opencv=run_opencv,
+            func_scipy=run_scipy
         )()
